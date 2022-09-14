813b615457c6492413921d2c08cbcf4a0f3523ae
Klebert-01 <arvid.berkert@gmail.com>
2022-09-14 13:07:39 +0200

    glömde spara ner filen, ny 13.07

--- 14Sept/Program.cs
+++ 14Sept/Program.cs
@@ -1,7 +1,2 @@
-﻿// 14 Sept
+﻿// Objektorienterad programmering, Konstruktor och inkapsling
 
-//lägger till en ny textrad via github, ska prova att använda pull kommandot och uppdatera mitt lokala
-
-//vi lägger till en ny kommentar, 10.09
-
-//den här kommentaren ska sparas i min nya branch arvid
